9a9cab6e2afce71f6512bb44a43dbff346513aac
luis cahuana <luisrolandocahuanaleon@gmail.com>
2024-10-08 21:50:03 +0200

    Autofixture installed

--- FootballScoreboard.Tests/ScoreboardAutofixtureTest.cs
+++ FootballScoreboard.Tests/ScoreboardAutofixtureTest.cs
@@ -4,8 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using AutoFixture;
+
 namespace FootballScoreboard.Tests;
 
 public class ScoreboardAutofixtureTest
 {
+    private readonly Fixture fixture;
+
 }
